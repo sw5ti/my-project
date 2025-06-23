04f449f6324ab0304cdb681a1fd875f37eba6e0e
Sw5ti <swadity.sk54@gmail.com>
2025-06-23 05:48:24 +0000

    hello

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -8,6 +8,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Starting Spring Boot Application...");
+		System.out.println("hello");
 		SpringApplication.run(Main.class, args);
 	}
 
